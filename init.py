cba6120eda7aaccb1ecbc19887e16891e96a566c
Kartik <saxenakartik007>
2017-10-11 21:37:27 +0530

    captcha process function call

--- init.py
+++ init.py
@@ -2,6 +2,7 @@ from pyvirtualdisplay import Display
 import selenium.webdriver.support.ui as ui
 import selenium.webdriver as webdriver
 import os
+from Image_Converter import processCaptcha
 
 
 display = Display(visible=0, size=(800, 600))
@@ -16,6 +17,8 @@ try:
     captcha_image=browser.find_element_by_id("ctl00_SPWebPartManager1_g_d6d774b9_498e_4de6_8690_a93e944cbeed_ctl00_imgCaptcha")
     src = captcha_image.get_attribute("src")
     print "src "+src
+    captcha = processCaptcha(src)
+    print captcha
 finally:
     #tidy-up
     browser.quit()
