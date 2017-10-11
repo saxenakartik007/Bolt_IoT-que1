28ed73ae6a0022b0787c61c42910fa34bb1d2e43
Kartik <saxenakartik007>
2017-10-11 21:33:07 +0530

    chrome driver added in project directory

--- init.py
+++ init.py
@@ -1,6 +1,7 @@
 from pyvirtualdisplay import Display
 import selenium.webdriver.support.ui as ui
 import selenium.webdriver as webdriver
+import os
 
 
 display = Display(visible=0, size=(800, 600))
@@ -9,7 +10,7 @@ cons_id=raw_input("Tracking id: ")
 data={}
 data["tracking id"]=cons_id
 try:
-    browser = webdriver.Chrome("/home/administrator/Downloads/chromedriver")
+    browser = webdriver.Chrome(os.path.realpath("chromedriver"))
     browser.get('https://www.indiapost.gov.in/VAS/Pages/trackconsignment.aspx')
     wait = ui.WebDriverWait(browser, 10)
     captcha_image=browser.find_element_by_id("ctl00_SPWebPartManager1_g_d6d774b9_498e_4de6_8690_a93e944cbeed_ctl00_imgCaptcha")
